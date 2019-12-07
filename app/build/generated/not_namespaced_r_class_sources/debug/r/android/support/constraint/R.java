ffdc584a86f202bcc4b380178b0b11696664e75f
specyin <specyin@gmail.com>
2019-12-07 13:53:22 +0530

    BETA 1.0.0.7 - Added Users

--- app/build/generated/not_namespaced_r_class_sources/debug/r/android/support/constraint/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/r/android/support/constraint/R.java
@@ -73,20 +73,20 @@ public final class R {
     public static final class id {
         private id() {}
 
-        public static final int bottom = 0x7f090027;
-        public static final int end = 0x7f09004b;
-        public static final int gone = 0x7f09005b;
-        public static final int invisible = 0x7f090069;
-        public static final int left = 0x7f09006e;
-        public static final int packed = 0x7f090086;
-        public static final int parent = 0x7f090088;
-        public static final int percent = 0x7f09008b;
-        public static final int right = 0x7f090090;
-        public static final int spread = 0x7f0900b7;
-        public static final int spread_inside = 0x7f0900b8;
-        public static final int start = 0x7f0900bd;
-        public static final int top = 0x7f0900d6;
-        public static final int wrap = 0x7f0900ec;
+        public static final int bottom = 0x7f090029;
+        public static final int end = 0x7f09004d;
+        public static final int gone = 0x7f09005d;
+        public static final int invisible = 0x7f09006b;
+        public static final int left = 0x7f090072;
+        public static final int packed = 0x7f09008b;
+        public static final int parent = 0x7f09008d;
+        public static final int percent = 0x7f090090;
+        public static final int right = 0x7f090095;
+        public static final int spread = 0x7f0900c0;
+        public static final int spread_inside = 0x7f0900c1;
+        public static final int start = 0x7f0900c6;
+        public static final int top = 0x7f0900df;
+        public static final int wrap = 0x7f0900f5;
     }
     public static final class styleable {
         private styleable() {}
--- app/src/main/java/com/kirg/vicon/Shivaram.java
+++ app/src/main/java/com/kirg/vicon/Shivaram.java
@@ -1,7 +1,11 @@
 package com.kirg.vicon;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Shivaram extends AppCompatActivity {
 
@@ -9,5 +13,115 @@ public class Shivaram extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shivaram);
+
+        ImageView shivaramli =findViewById(R.id.shivaramli);
+        ImageView specyin =findViewById(R.id.specyin);
+        ImageView janakili =findViewById(R.id.janakili);
+        ImageView shreyali =findViewById(R.id.shreyali);
+        ImageView saikiranli =findViewById(R.id.saikiranli);
+        ImageView bhavanali =findViewById(R.id.bhavanali);
+        ImageView jahnavili =findViewById(R.id.jahnavili);
+        ImageView amulli =findViewById(R.id.amulli);
+        ImageView luma =findViewById(R.id.luma);
+
+        /** Shivaram Linked In **/
+        shivaramli.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Check for network connectivity
+                Uri uri = Uri.parse("https://www.linkedin.com/in/shivaramjimada/"); // missing 'http://' will cause crashed
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+
+            }
+        });
+        /** Shivaram Website In **/
+        specyin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Check for network connectivity
+                Uri uri = Uri.parse("https://www.specy.in"); // missing 'http://' will cause crashed
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+
+            }
+        });
+        /** janaki Li In **/
+        janakili.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Check for network connectivity
+                Uri uri = Uri.parse("https://www.linkedin.com/in/mellacheruvu-janakiram-a1bbbb178/"); // missing 'http://' will cause crashed
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+
+            }
+        });
+
+        shreyali.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Check for network connectivity
+                Uri uri = Uri.parse("https://www.linkedin.com/in/indukuri-shreya-b4062b187"); // missing 'http://' will cause crashed
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+
+            }
+        });
+
+        saikiranli.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Check for network connectivity
+                Uri uri = Uri.parse("https://www.linkedin.com/in/vududala-sai-kiran/"); // missing 'http://' will cause crashed
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+
+            }
+        });
+        bhavanali.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Check for network connectivity
+                Uri uri = Uri.parse("https://www.linkedin.com/in/doulaghar-bhavana-332561199/"); // missing 'http://' will cause crashed
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+
+            }
+        });
+
+        jahnavili.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Check for network connectivity
+                Uri uri = Uri.parse("https://www.linkedin.com/in/mellacheruvu-janakiram-a1bbbb178/"); // missing 'http://' will cause crashed
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+
+            }
+        });
+
+        amulli.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Check for network connectivity
+                Uri uri = Uri.parse("https://www.linkedin.com/in/praneet-amul-akash-cherukuri/"); // missing 'http://' will cause crashed
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+
+            }
+        });
+
+        luma.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Check for network connectivity
+                Uri uri = Uri.parse("https://sites.google.com/view/praneet-amul-cherukuri/"); // missing 'http://' will cause crashed
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+
+            }
+        });
+
     }
 }
