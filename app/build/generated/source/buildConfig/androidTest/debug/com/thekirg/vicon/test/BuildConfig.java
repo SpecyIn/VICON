35a0f280d6289e3a26819ef691a36ec90195066a
specyin <specyin@gmail.com>
2019-12-09 21:41:10 +0530

    Release 1.2.1 - Added FIRE BASE

--- app/build/generated/source/buildConfig/androidTest/debug/com/thekirg/vicon/test/BuildConfig.java
+++ app/build/generated/source/buildConfig/androidTest/debug/com/thekirg/vicon/test/BuildConfig.java
@@ -5,7 +5,7 @@ package com.thekirg.vicon.test;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "com.kirg.vicon.test";
+  public static final String APPLICATION_ID = "com.thekirg.vicon.test";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
