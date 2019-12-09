5e7a8085c20093608d3cf0432f506b108201f690
specyin <specyin@gmail.com>
2019-12-09 23:01:16 +0530

    Release 1.2.2 - New Update

--- app/src/main/java/com/thekirg/vicon/SearchUI.java
+++ app/src/main/java/com/thekirg/vicon/SearchUI.java
@@ -7,10 +7,12 @@ import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
--- app/src/main/java/com/thekirg/vicon/preload.java
+++ app/src/main/java/com/thekirg/vicon/preload.java
@@ -2,7 +2,7 @@ package com.thekirg.vicon;
 
 import android.content.Intent;
 import android.os.CountDownTimer;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class preload extends AppCompatActivity {
